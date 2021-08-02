21078389ce6674272e08bc7cef2998c6e29690cd
Dion Kim <dev.dionkim@gmail.com>
2021-08-02 22:35:45 +0900

    4장 다 봤다

--- ch04/ex4-7.go
+++ ch04/ex4-7.go
@@ -4,7 +4,7 @@
 var array1 [5]string
 
 // 다섯 개의 원소를 가지는 두 번째 문자열 배열을 선언한다.
-// 이 배열을 색상을 표현하는 만자열로 초기화한다.
+// 이 배열을 색상을 표현하는 문자열로 초기화한다.
 array2 := [5]string{"Red", "Blue", "Green", "Yello", "Pink"}
 
 // array2의 값들을 array1로 복사한다.
